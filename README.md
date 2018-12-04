38bec8359e214784c57865dd18f343a8b0c7c7fa
danimtb <danimanzaneque@gmail.com>
2018-12-04 11:30:30 +0100

    update to 2.2.6

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 # conan-<name>
 
-![conan-<name> image](/images/conan-<name>.png)
+![conan-muparser image](/images/conan-muparser.png)
 
-[![Download](https://api.bintray.com/packages/conan-community/conan/<name>%3Aconan/images/download.svg?version=<version>%3Astable)](https://bintray.com/conan-community/conan/<name>%3Aconan/<version>%3Astable/link)
-[![Build Status](https://travis-ci.org/conan-community/conan-<name>.svg?branch=stable%2F<version>)](https://travis-ci.org/conan-community/conan-<name>)
-[![Build status](https://ci.appveyor.com/api/projects/status/jyeh443gn0l0f3bi/branch/stable/<version>?svg=true)](https://ci.appveyor.com/project/<appveyor_user>/conan-<name>/branch/stable/<version>)
+[![Download](https://api.bintray.com/packages/conan-community/conan/<name>%3Aconan/images/download.svg?version=2.2.6%3Astable)](https://bintray.com/conan-community/conan/<name>%3Aconan/2.2.6%3Astable/link)
+[![Build Status](https://travis-ci.org/conan-community/conan-<name>.svg?branch=stable%2F2.2.6)](https://travis-ci.org/conan-community/conan-<name>)
+[![Build status](https://ci.appveyor.com/api/projects/status/jyeh443gn0l0f3bi/branch/stable/2.2.6?svg=true)](https://ci.appveyor.com/project/<appveyor_user>/conan-<name>/branch/stable/2.2.6)
 
 [Conan.io](https://conan.io) package for [<name>](<homepage>) project.
 
-The packages generated with this *conanfile.py* can be found in [Bintray](https://bintray.com/conan-community/conan/<name>%3Aconan).
+The packages generated with this *conanfile.py* can be found in [Bintray](https://bintray.com/conan-community/conan/muparser%3Aconan).
 
 ## Basic setup
 
-    $ conan install <name>/<version>@conan/stable
+    $ conan install <name>/2.2.6@conan/stable
 
 ## Project setup
 
 If you handle multiple dependencies in your project is better to add a *conanfile.txt*:
 
     [requires]
-    <name>/<version>@conan/stable
+    <name>/2.2.6@conan/stable
 
     [generators]
     txt
--- conanfile.py
+++ conanfile.py
@@ -1,37 +1,33 @@
 import os
-import shutil
 from conans import ConanFile, tools, CMake
 
 
-class RapiXMLConan(ConanFile):
+class MuparserConan(ConanFile):
     name = "muparser"
-    version = "2.2.5"
+    version = "2.2.6"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = {"shared": False}
     description = "Fast Math Parser Library"
     url = "https://github.com/conan-community/conan-muparser"
     homepage = "http://beltoforion.de/article.php?a=muparser"
     license = "MIT"
     exports = ["LICENSE.md"]
-    exports_sources = ["CMakeLists.txt"]
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
 
     def source(self):
         tools.get("https://github.com/beltoforion/%s/archive/v%s.zip" % (self.name, self.version))
-        os.rename(self.name + "-" + self.version, self.source_subfolder)
-        shutil.copy2("CMakeLists.txt", self.source_subfolder)
-        os.unlink("CMakeLists.txt")
+        os.rename(self.name + "-" + self.version, self._source_subfolder)
 
     def build(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        cmake.configure(source_folder=self.source_subfolder)
+        cmake.configure(source_folder=self._source_subfolder)
         cmake.build()
 
     def package(self):
-        self.copy(pattern="License.txt", src=self.source_subfolder, dst="licenses")
-        self.copy(pattern="*.h", src="%s/include" % self.source_subfolder, dst="include")
+        self.copy(pattern="License.txt", src=self._source_subfolder, dst="licenses")
+        self.copy(pattern="*.h", src="%s/include" % self._source_subfolder, dst="include")
         self.copy(pattern="*.lib", src=str(self.settings.build_type), dst="lib")
         self.copy(pattern="*.dll", src=str(self.settings.build_type), dst="bin")
         self.copy(pattern="libmuparser.dll.a", dst="lib")
