0fa15798ef97aa3efadfc8e0f7d19fd97cd8d03c
SSE4 <tomskside@gmail.com>
2018-12-09 01:40:46 +0700

    - use CMake wrapper to ensure conan settings are applied
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -29,6 +29,12 @@ class MuparserConan(ConanFile):
                   sha256="daf4a937abdc33b361d4a2fbc79bf311d5486ebc87c56596130e295db1302303")
         os.rename(self.name + "-" + self.version, self._source_subfolder)
 
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'CMakeLists.txt'),
+                              'cmake_minimum_required (VERSION 3.1.0)',
+                              'cmake_minimum_required (VERSION 3.1.0)\n'
+                              'include(conanbuildinfo.cmake)\n'
+                              'conan_basic_setup()')
+
     def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
